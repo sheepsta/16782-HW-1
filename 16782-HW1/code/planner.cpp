c052d656d25606920d2987a6edd3c66c339cbf52
Julius Arolovitch <81655538+sheepsta@users.noreply.github.com>
2024-02-14 16:26:17 -0500

    Update planner.cpp

--- 16782-HW1/code/planner.cpp
+++ 16782-HW1/code/planner.cpp
@@ -6,8 +6,10 @@
 #include "planner.h"
 #include <math.h>
 #include <cstdio>
+#include <chrono>
+#include <queue>
 
-#define GETMAPINDEX(X, Y, XSIZE, YSIZE) ((Y - 1) * XSIZE + (X - 1))
+#define GETMAPINDEX(X, Y, XSIZE, YSIZE) ((Y) * XSIZE + (X))
 
 #if !defined(MAX)
 #define MAX(A, B) ((A) > (B) ? (A) : (B))
@@ -33,8 +35,25 @@ struct queue_node
 {
     int x;
     int y;
-    queue_node *next;
+    float cost;
+    float heuristic;
+    float priority;
 };
+
+struct CompareNode
+{
+    bool operator()(const queue_node *lhs, const queue_node *rhs) const
+    {
+        return lhs->priority > rhs->priority;
+    }
+};
+
+float euclidean_distance(int x1, int y1, int x2, int y2)
+{
+    return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+}
+
+using MinHeap = std::priority_queue<queue_node *, std::vector<queue_node *>, CompareNode>;
 
 // Struct to represent each position on the cost map
 struct map_node
@@ -53,70 +72,14 @@ struct cmap
     map_node **map;
 };
 
-// Struct to represent the queue
-struct queue
-{
-    queue_node *first;
-    queue_node *last;
-    int size;
-};
-
-// Check if queue empty
-bool queue_empty(queue *q)
-{
-    return (q->size == 0);
-}
-
-// Push node to queue
-void push(queue *q, queue_node *n)
-{
-    if (queue_empty(q))
-    {
-        q->first = n;
-        q->last = n;
-    }
-    else
-    {
-        n->next = NULL;
-        q->last->next = n;
-        q->last = n;
-    }
-    q->size++;
-}
-
-// Pop node from queue
-queue_node *pop(queue *q)
-{
-    if (!queue_empty(q))
-    {
-        queue_node *n = q->first;
-        q->first = q->first->next;
-        q->size--;
-        return n;
-    }
-    else
-    {
-        return NULL;
-    }
-}
-
-// Returns a new queue
-queue *queue_new()
-{
-    queue *q = new queue;
-    q->first = NULL;
-    q->last = NULL;
-    q->size = 0;
-    return q;
-}
-
-// Returns a new node corresponding to x, y
-queue_node *queue_node_new(int x, int y)
+queue_node *queue_node_new(int x, int y, float cost, int goalX, int goalY)
 {
     queue_node *n = new queue_node;
     n->x = x;
     n->y = y;
-    n->next = NULL;
+    n->cost = cost;
+    n->heuristic = euclidean_distance(x, y, goalX, goalY);
+    n->priority = n->cost + n->heuristic;
     return n;
 }
 
@@ -152,15 +115,6 @@ void free_cmap(cmap *c, int x_size)
     delete c;
 }
 
-// Clears the queue
-void clear_queue(queue *q)
-{
-    while (!queue_empty(q))
-    {
-        delete pop(q);
-    }
-}
-
 // Checks whether x, y is a valid position in the map and free of obstacles
 bool valid_pos(int x, int y, int x_size, int y_size, int *map, int collision_thresh)
 {
@@ -168,16 +122,21 @@ bool valid_pos(int x, int y, int x_size, int y_size, int *map, int collision_thr
 }
 
 // Returns the cost map given parameters
-cmap *fill_cmap(int *map, int *target_traj, int curr_time, int target_steps, int collision_thresh, int x_size, int y_size)
+cmap *fill_cmap(int *map, int *target_traj, int curr_time, int target_steps, int collision_thresh, int x_size, int y_size, int robotPoseX, int robotPoseY)
 {
+
+    MinHeap q;
+
+    auto start = std::chrono::high_resolution_clock::now(); // Start timing
+
     int dX[NUMOFDIRS] = {-1, -1, -1, 0, 0, 1, 1, 1};
     int dY[NUMOFDIRS] = {-1, 0, 1, -1, 1, -1, 0, 1};
 
     cmap *c = cmap_new(x_size, y_size);
-    queue *q = queue_new();
 
     for (int i = curr_time; i < target_steps; i++)
     {
+
         int x_targ_step = target_traj[i];
         int y_targ_step = target_traj[target_steps + i];
         c->map[x_targ_step - 1][y_targ_step - 1].best_x = x_targ_step;
@@ -186,12 +145,19 @@ cmap *fill_cmap(int *map, int *target_traj, int curr_time, int target_steps, int
         c->map[x_targ_step - 1][y_targ_step - 1].cost = 0;
         c->map[x_targ_step - 1][y_targ_step - 1].expanded = true;
         c->map[x_targ_step - 1][y_targ_step - 1].idx_of_path_node = i;
-        push(q, queue_node_new(x_targ_step, y_targ_step));
+        q.push(queue_node_new(x_targ_step, y_targ_step, c->map[x_targ_step - 1][y_targ_step - 1].cost, robotPoseX, robotPoseY));
     }
 
-    while (!queue_empty(q))
+    while (!q.empty())
     {
-        queue_node *n = pop(q);
+        auto now = std::chrono::high_resolution_clock::now();
+        double elapsed = std::chrono::duration<double>(now - start).count();
+        if (elapsed > 0.95)
+        {
+            return c;
+        }
+        queue_node *n = q.top();
+        q.pop();
         int curr_x = n->x;
         int curr_y = n->y;
         delete n;
@@ -218,39 +184,24 @@ cmap *fill_cmap(int *map, int *target_traj, int curr_time, int target_steps, int
                         c->map[new_x - 1][new_y - 1].cost = c->map[curr_x - 1][curr_y - 1].cost + map[GETMAPINDEX(new_x, new_y, x_size, y_size)];
                         c->map[new_x - 1][new_y - 1].steps = c->map[curr_x - 1][curr_y - 1].steps + 1;
                         c->map[new_x - 1][new_y - 1].idx_of_path_node = c->map[curr_x - 1][curr_y - 1].idx_of_path_node;
-                        push(q, queue_node_new(new_x, new_y));
+                        q.push(queue_node_new(new_x, new_y, c->map[new_x - 1][new_y - 1].cost, robotPoseX, robotPoseY));
                     }
                     else
                     {
-                        // If we have found a new way to reach this node in less steps
-                        if (c->map[new_x - 1][new_y - 1].steps > c->map[curr_x - 1][curr_y - 1].steps + 1)
+                        bool cost_condition = c->map[new_x - 1][new_y - 1].cost > c->map[curr_x - 1][curr_y - 1].cost + map[GETMAPINDEX(new_x, new_y, x_size, y_size)];
+                        if (cost_condition)
                         {
-                            c->map[new_x - 1][new_y - 1].steps = c->map[curr_x - 1][curr_y - 1].steps + 1;
                             c->map[new_x - 1][new_y - 1].cost = c->map[curr_x - 1][curr_y - 1].cost + map[GETMAPINDEX(new_x, new_y, x_size, y_size)];
                             c->map[new_x - 1][new_y - 1].best_x = curr_x;
                             c->map[new_x - 1][new_y - 1].best_y = curr_y;
                             c->map[new_x - 1][new_y - 1].idx_of_path_node = c->map[curr_x - 1][curr_y - 1].idx_of_path_node;
-                            push(q, queue_node_new(new_x, new_y));
-                        }
-                        // If we have found a new way to reach this node with less cost
-                        else if (c->map[new_x - 1][new_y - 1].steps == c->map[curr_x - 1][curr_y - 1].steps + 1)
-                        {
-                            if (c->map[new_x - 1][new_y - 1].cost > c->map[curr_x - 1][curr_y - 1].cost + map[GETMAPINDEX(new_x, new_y, x_size, y_size)])
-                            {
-                                c->map[new_x - 1][new_y - 1].cost = c->map[curr_x - 1][curr_y - 1].cost + map[GETMAPINDEX(new_x, new_y, x_size, y_size)];
-                                c->map[new_x - 1][new_y - 1].best_x = curr_x;
-                                c->map[new_x - 1][new_y - 1].best_y = curr_y;
-                                c->map[new_x - 1][new_y - 1].idx_of_path_node = c->map[curr_x - 1][curr_y - 1].idx_of_path_node;
-                                push(q, queue_node_new(new_x, new_y));
-                            }
+                            q.push(queue_node_new(new_x, new_y, c->map[new_x - 1][new_y - 1].cost, robotPoseX, robotPoseY));
                         }
                     }
                 }
             }
         }
     }
-    delete q;
-
     return c;
 }
 
@@ -284,18 +235,20 @@ void planner(
     }
 
     // Fill the cost map
-    cmap *c = fill_cmap(map, target_traj, curr_time, target_steps, collision_thresh, x_size, y_size);
+    cmap *c = fill_cmap(map, target_traj, curr_time, target_steps, collision_thresh, x_size, y_size, robotposeX, robotposeY);
 
     // If our current position has been expanded (i.e. we can feasibly reach a point on the target trajectory from it)
     // Move to the best x, y calculated during the cmap fill
     if (c->map[robotposeX - 1][robotposeY - 1].expanded)
     {
+        printf("expanded\n");
         action_ptr[0] = c->map[robotposeX - 1][robotposeY - 1].best_x;
         action_ptr[1] = c->map[robotposeX - 1][robotposeY - 1].best_y;
     }
     // We can't catch the target, and just stay still
     else
     {
+        printf("not expanded\n");
         action_ptr[0] = robotposeX;
         action_ptr[1] = robotposeY;
     }
